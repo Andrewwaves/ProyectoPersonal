d3963e43f19bbed3a294cffd4d899e5560bd7f07
cie <andreacanalesmartin2000@gmail.com>
2025-03-13 12:09:25 +0100

    back

---
+++ src/main/java/es/cie/ordenador/repositories/OrdenadorRowMapper.java
@@ -0,0 +1,21 @@
+package es.cie.ordenador.repositories;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+
+import es.cie.ordenador.negocio.Ordenador;
+
+public class OrdenadorRowMapper implements RowMapper<Ordenador>{
+
+    @Override
+    public Ordenador mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Ordenador(rs.getInt("numero"),
+        rs.getString("marca"),rs.getString("modelo"),
+        rs.getInt("precio"));
+    }
+
+    
+}
